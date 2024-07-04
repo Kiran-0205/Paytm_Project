3be63beedeb162122cab83ab77a6350413b038b9
Kiran <saikiranreddyporeddy@gmail.com>
2024-07-04 12:27:31 +0530

    mongo & db.js setup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
+const app = express();
+const PORT = 3000;
 
+app.get('/paytmProject', (req, res) => {
+    res.send("This is a paytm project");
+})
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
